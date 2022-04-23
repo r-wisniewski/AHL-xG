48ce30f832600525b0414a85ae09cb452747ff85
Robin Wisniewski <wisniewski.ro@gmail.com>
2022-04-23 10:29:15 -0700

    multithreaded accuracy script
    similar multithreading to the scraper

--- ahl_xgf_sql_accuracy.py
+++ ahl_xgf_sql_accuracy.py
@@ -14,14 +14,20 @@ import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 import pandas
+from requests.sessions import Session
+from concurrent.futures import ThreadPoolExecutor
+from threading import local
+from tqdm import tqdm
 
 #build dataset with data from first game w/ shot and goal data
 #Game is from Oct 6, 2017
 first_game_id = 1017122
 
 #callbacks._8 includes all the shot/goal data including x,y coordinates etc...
+#callbacks._5 also works
 url = 'https://lscluster.hockeytech.com/feed/index.php?feed=statviewfeed&view=gameCenterPlayByPlay&game_id='
 end ='&key=50c2cd9b5e18e390&client_code=ahl&lang=en&league_id=&callback=angular.callbacks._8'
+thread_local = local()
 
 # CONNECT TO POSTGRES DB #
 try:
@@ -67,36 +73,41 @@ try:
 except (Exception, psycopg2.DatabaseError) as error :
     print ("Error while creating PostgreSQL table", error)
 
-for n in range(first_game_id,last_game+1,1):
-    ######### Loop through the game summaries ###########
-    
-    fullurl = url + str(n) + end
-    
-    resp = requests.get(fullurl)
-        
-    #format resp and resp2 to be json responses. Cut the beginning and end portions off
-    resp = resp.text[:-1]
-    resp = resp[21:]
-    #this response is json
-    try:
-        resp = json.loads(resp)
+def make_url_list() -> list:
+    url_list = []
+    for n in range(first_game_id,last_game+1,1):
+        fullurl = url + str(n) + end
+        url_list.append(fullurl)
+    return url_list
 
-        resp1 = resp
-        resp2 = resp
+def request_all(url_list:list) -> None:
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        executor.map(request_url,url_list)
+
+def get_session() -> Session:
+    if not hasattr(thread_local,'session'):
+        thread_local.session = requests.Session()
+    return thread_local.session
+
+def request_url(url:str) -> None:
+    session = get_session()
+    with session.get(url) as resp:
+        resp = resp.text[:-1]
+        resp = resp[21:]
+        json_data = json.loads(resp)
+        bar.update(1)
 
         #Team1 is the team that the strengths will be calculated for
         #Team2 is just the inverse of Team1
-        #ie, if Team1 is on the PP (strength = +1), Team2 must have strength == -1 (5v4 PK)
+        #ie, if Team1 is on the PP (strength = +1), Team2 must have the inverse strength == -1 (5v4 PK)
         team = []
-        for i in resp:
-            event = i.get("event")
+        for i in json_data:
+            event = i['event']
             if event == "goalie_change":
-                details = i.get("details")
-                team_id = details.get("team_id")
+                team_id = i['details']['team_id']
                 team.append(team_id) 
         Team1 = int(team[0])
         Team2 = int(team[1])
-
         # Create and populate array with strengths
         # 3 20 min periods plus possible 5 min OT (for regular season) = 65 min * 60 secs = 3900 secs
         # gametime == [0,1,2.......,3899]
@@ -106,16 +117,14 @@ for n in range(first_game_id,last_game+1,1):
         #in time (for Team1)
         # Access as: strengths[row,col]
         strengths = np.vstack((gametime, zero_array)).T
-
         total_expected_goals = 0
         game_goals = 0
-
         # First run through the events, we create and populate the strength array
         # Strengths are calculated as the advantage or disadvantage of Team 1
         # ie, +1 can indicate 5v4 OR 4v3 in favor of Team1
-        for i in resp1:
+        for i in json_data:
             #get the event type and assume we have all metadata
-            event = i.get("event")
+            event = i['event']
 
             ######### calculate strengths at each moment in time ########
 
@@ -123,18 +132,14 @@ for n in range(first_game_id,last_game+1,1):
             last_penalty_time = 0
 
             if event == "penalty":
-                details = i.get("details")
-                time_raw = details.get("time").split(":")
-                period = details.get("period")
-                period = int(period.get("id"))
+                time_raw = i['details']['time'].split(":")
+                period = int(i['details']['period']['id'])
                 #get time in seconds of the penalty
                 time = ((period-1) * 20 * 60) + (int(time_raw[0]) * 60) + int(time_raw[1])
-                length = details.get("minutes").split(".")
+                length = i['details']['minutes'].split(".")
                 length = int(length[0])
 
-                against_team = details.get("againstTeam")
-                against_team = against_team.get("id")
-                against_team = int(against_team)
+                against_team = int(i['details']['againstTeam']['id'])
                 #Team1 goes on the PP
                 if against_team == Team2:
                     if length == 2:
@@ -162,19 +167,15 @@ for n in range(first_game_id,last_game+1,1):
                         for i in range(301):
                             if int(strengths[time+i,1]) > -2:
                                 strengths[time+i,1] -= 1
-
+            
             #we only care about PP goals becuase SH or EV goals don't change strength
             elif event == "goal":
-                details = i.get("details")
-                pp_goal = details.get("properties")
-                pp_goal = int(pp_goal.get("isPowerPlay"))
-                team_id = details.get("team")
-                team_id = int(team_id.get("id"))
+                pp_goal = int(i['details']['properties']['isPowerPlay'])
+                team_id = int(i['details']['team']['id'])
                 if pp_goal == 1:
                     #time of goal in seconds
-                    time_raw = details.get("time").split(":")
-                    period = details.get("period")
-                    period = int(period.get("id"))
+                    time_raw = i['details']['time'].split(":")
+                    period = int(i['details']['period']['id'])
                     time_of_goal = ((period-1) * 20 * 60) + (int(time_raw[0]) * 60) + int(time_raw[1])
 
                     #time of previous penalty + length of penalty is last_penalty_time
@@ -190,20 +191,16 @@ for n in range(first_game_id,last_game+1,1):
                         for i in range(time_of_goal+1,last_penalty_time+1):
                             if int(strengths[i,1]) < 2:
                                 strengths[i,1] += 1
-        # 2nd run through the events. Stengths are calculated. Now we must figure out whether the even is
-        # a goal or shot. Find the xG of the event based on strength and then add to ahlxgaccuracy table.
-        for i in resp2:
-            #get the event type and assume we have all metadata
-            event = i.get("event")
 
+        # 2nd run through the events, we get the goal and combine that with strength. Add to SQL DB.
+        for i in json_data:
+            event = i['event']
             if event == "goal":
-                details = i.get("details")
-
                 #set the X value relative to x distance from the net
                 # this is because the home team's x value is (max X)/2 + true X
                 # max x = 593/2
                 # X value is out towards the blue line
-                xLocation = details.get("xLocation")
+                xLocation = i['details']['xLocation']
                 if xLocation > 593/2:
                     xLocation = 593 - xLocation
                 else:
@@ -211,17 +208,15 @@ for n in range(first_game_id,last_game+1,1):
                 #get the Y value as a Y distance relative to the net
                 #largest Y val is 300.
                 #since origin is at top left we must flip all the y values
-                yLocation = details.get("yLocation")
+                yLocation = i['details']['yLocation']
                 yLocation = 300 - yLocation
 
                 #get the team id of the goal scorer
-                team_id = details.get("team")
-                team_id = int(team_id.get("id"))
+                team_id = int(i['details']['team']['id'])
 
                 #get time of goal and match to strength
-                time_raw = details.get("time").split(":")
-                period = details.get("period")
-                period = int(period.get("id"))
+                time_raw = i['details']['time'].split(":")
+                period = int(i['details']['period']['id'])
                 event_time = ((period-1) * 20 * 60) + (int(time_raw[0]) * 60) + int(time_raw[1])
 
                 #if the team who shot on net or scored is Team1, set strength equal to the strength array 
@@ -241,15 +236,13 @@ for n in range(first_game_id,last_game+1,1):
                 expected_goals = cursor.fetchone()
                 total_expected_goals += expected_goals[0]
                 game_goals += 1
-            
-            elif event == "shot":
-                details = i.get("details")
 
+            elif event == "shot":
                 #set the X value relative to x distance from the net
                 # this is because the home team's x value is (max X)/2 + true X
                 # max x = 593/2
                 # X value is out towards the blue line
-                xLocation = details.get("xLocation")
+                xLocation = i['details']['xLocation']
                 if xLocation > 593/2:
                     xLocation = 593 - xLocation
                 else:
@@ -257,16 +250,15 @@ for n in range(first_game_id,last_game+1,1):
                 #get the Y value as a Y distance relative to the net
                 #largest Y val is 300.
                 #since origin is at top left we must flip all the y values
-                yLocation = details.get("yLocation")
+                yLocation = i['details']['yLocation']
                 yLocation = 300 - yLocation
 
                 #get the team id of the shooter
-                team_id = int(details.get("shooterTeamId"))
+                team_id = int(i['details']['shooterTeamId'])
 
                 #get time of goal or shot and match to strength
-                time_raw = details.get("time").split(":")
-                period = details.get("period")
-                period = int(period.get("id"))
+                time_raw = i['details']['time'].split(":")
+                period = int(i['details']['period']['id'])
                 event_time = ((period-1) * 20 * 60) + (int(time_raw[0]) * 60) + int(time_raw[1])
 
                 #if the team who shot on net or scored is Team1, set strength equal to the strength array 
@@ -288,13 +280,20 @@ for n in range(first_game_id,last_game+1,1):
         
         #insert the games data into the ahlxgfaccuracy table. One entry per game
         diff = total_expected_goals - game_goals
+        n = url.split("=")
+        n = n[3].split("&")[0]
         values = """ INSERT INTO ahlxgfaccuracy (GameID, xG, ActualGoals, Difference) VALUES (%s,%s,%s,%s) """
         cursor.execute(values, (n,total_expected_goals,game_goals,diff))
         connection.commit()
-        print("Game ID: %i, xG: %f, Goals: %i, Diff: %f" % (n,total_expected_goals,game_goals,diff))
-    except:
-        pass
-        
+
+global bar 
+bar = tqdm(desc="Progress",total=(last_game-first_game_id))
+
+url_list = make_url_list()
+request_all(url_list)
+
+bar.close()
+
 #Calculate Avg goals/game error and MSE
 hist_list = []
 cursor.execute("SELECT * from ahlxgfaccuracy;")
@@ -317,7 +316,6 @@ plt.title('Histogram of the error (goals/game)')
 plt.hist(hist_list)
 plt.axvline(avg_error, color='r', linestyle='dashed', linewidth=1)
 plt.show()
-
 
 #closing database connection.
 if(connection):
